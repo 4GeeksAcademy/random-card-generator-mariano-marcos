9f762c3a72fe3fdcb2c94e18127c398506d9f052
Mariano Marcos <140490758+Zavanne@users.noreply.github.com>
2024-05-31 23:29:05 +0000

    first version

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["♦", "♥", "♠", "♣"];
+  let ranks = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let cardSuits = document.querySelectorAll(".card-suit");
+  let suitIndex = Math.floor(Math.random() * suits.length);
+  let suitColor = suitIndex === 0 || suitIndex === 1 ? "red" : "black";
+  console.log("Selected suit:", suits[suitIndex]); // Log the selected suit
+
+  cardSuits.forEach(cardSuit => {
+    cardSuit.innerHTML = suits[suitIndex];
+    cardSuit.style.color = suitColor;
+  });
+
+  let cardValue = document.getElementById("card-value");
+  let cardValueIndex = Math.floor(Math.random() * ranks.length);
+  cardValue.innerHTML = ranks[cardValueIndex];
+  cardValue.style.color = suitColor;
 };
